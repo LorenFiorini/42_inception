67864bfa53bdd3cdbcad4b0da12f93e69567d3e9
LorenFiorini <lorenfiorini@gmail.com>
2024-01-27 00:55:09 +0100

    MariaDB server configuration

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-echo "lol"
+echo "lol" # Debug
+
+
+# ls etc/mysql/mariadb.conf.d                 # Debug
+# cat etc/mysql/mariadb.conf.d/50-server.cnf  # Debug
+
 # service mysql start
 
 
 # service mysql stop
+
+
