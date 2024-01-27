4735507cceb640ad7a2d0730117058201944caf4
LorenFiorini <lorenfiorini@gmail.com>
2024-01-27 01:02:46 +0100

    Previous server conf

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -3,8 +3,8 @@
 echo "lol" # Debug
 
 
-# ls etc/mysql/mariadb.conf.d                 # Debug
-# cat etc/mysql/mariadb.conf.d/50-server.cnf  # Debug
+ls etc/mysql/mariadb.conf.d                 # Debug
+cat etc/mysql/mariadb.conf.d/50-server.cnf  # Debug
 
 # service mysql start
 
