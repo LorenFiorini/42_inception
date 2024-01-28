cd3619164feaa9934439a77fd6568a6b8d1f8d00
LorenFiorini <lfiorini@student.42heilbronn.de>
2024-01-28 20:23:32 +0100

    new error msg

--- srcs/requirements/mariadb/tools/my-script_4.sh
+++ srcs/requirements/mariadb/tools/my-script_4.sh
@@ -6,20 +6,20 @@ SQL_PWD="$MYSQL_PASSWORD"
 SQL_ROOT_PWD="$MYSQL_ROOT_PASSWORD"
 
 # Start MySQL service
-service mysql start
+service mariadb start
 
 sleep 1
 
 # Create database and user
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DB}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USR}\`@'localhost' IDENTIFIED BY '${SQL_PWD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DB}\`.* TO \`${SQL_USR}\`@'%' IDENTIFIED BY '${SQL_PWD}';"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DB}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USR}\`@'localhost' IDENTIFIED BY '${SQL_PWD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DB}\`.* TO \`${SQL_USR}\`@'%' IDENTIFIED BY '${SQL_PWD}';"
 
 sleep 1
 
 # Set root user password and flush privileges
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PWD}';"
-mysql -e "FLUSH PRIVILEGES;"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PWD}';"
+mariadb -e "FLUSH PRIVILEGES;"
 
 # Shutdown MySQL server
 mysqladmin -u root -p$SQL_ROOT_PWD shutdown
