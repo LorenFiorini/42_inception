a855e8063eb601ea95c5876dd30379e04cf9f22f
LorenFiorini <lfiorini@student.42heilbronn.de>
2024-01-28 16:20:18 +0100

    my scripts

---
+++ my_scripts/full_clean.sh
@@ -0,0 +1,11 @@
+
+
+docker stop $(docker ps -qa)
+
+docker rm $(docker ps -qa)
+
+docker rmi -f $(docker images -qa)
+
+docker volume rm $(docker volume ls -q)
+
+docker network rm $(docker network ls -q) 2>>/home/lfiorini/Desktop/traces/debug_02
