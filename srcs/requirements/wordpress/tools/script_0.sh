28a392c43211671f6945efd2d9ee27796e773b33
LorenFiorini <lfiorini@student.42heilbronn.de>
2024-01-29 07:35:12 +0100

    Wordpress progress

--- srcs/requirements/wordpress/tools/script_0.sh
+++ srcs/requirements/wordpress/tools/script_0.sh
@@ -3,6 +3,7 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
+
 ls -la /var/www # debug
 ls -la /var/www/wordpress # debug
 cat /var/www/wordpress/wp-config-sample.php # debug
