207cfe21a2d317e51f1f27fbd6033b15d7a681ff
LorenFiorini <lfiorini@student.42heilbronn.de>
2024-02-02 09:58:12 +0100

    wordpress, checking default config

--- test/01/tools/script_0.sh
+++ test/01/tools/script_0.sh
@@ -4,14 +4,20 @@ echo "lol" # Debug
 
 
 
-echo "ls -la /var/lib/php/"
-ls -la /var/lib/php/
+echo "ls -la /var/lib/php/sessions"
+ls -la /var/lib/php/sessions
 
 echo "ls -la /usr/local/bin/ "
 ls -la /usr/local/bin/ 
 
-echo "ls -la /etc/php"
-ls -la /etc/php
+echo "ls -la /var"
+ls -la /var
+
+echo "ls -la /etc/php/7.4/fpm/pool.d/"
+ls -la /etc/php/7.4/fpm/pool.d/
+
+echo "cat /etc/php/7.4/fpm/pool.d/www.conf"
+cat /etc/php/7.4/fpm/pool.d/www.conf
 
 # ls -la
 # cat /etc/hosts
