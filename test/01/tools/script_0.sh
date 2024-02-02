aaac56c9ef5918cdbd434c0a3df28ac003e179d0
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2024-02-02 09:33:14 +0100

    test 01

---
+++ test/01/tools/script_0.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "lol" # Debug
+
+
+
+echo "ls -la /var/lib/php/"
+ls -la /var/lib/php/
+
+echo "ls -la /usr/local/bin/ "
+ls -la /usr/local/bin/ 
+
+echo "ls -la /etc/php"
+ls -la /etc/php
+
+# ls -la
+# cat /etc/hosts
+
+
